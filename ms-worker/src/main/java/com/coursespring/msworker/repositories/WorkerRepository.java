3334eb98ab6cf9cd12eaf5a81f203af53499f69f
Wallace Oliveira <wallace.goularte@hotmail.com>
2022-01-27 16:18:44 -0300

    Criação do repositório Worker

---
+++ ms-worker/src/main/java/com/coursespring/msworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.coursespring.msworker.repositories;
+
+import com.coursespring.msworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
