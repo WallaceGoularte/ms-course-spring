b0c976cbad57dca9bc8771dba9c91eae6cf434af
Wallace Oliveira <wallace.goularte@hotmail.com>
2022-01-27 16:22:37 -0300

    Alteração para persistir dados na base H2 ao subir aplicação

--- ms-worker/src/main/java/com/coursespring/msworker/MsWorkerApplication.java
+++ ms-worker/src/main/java/com/coursespring/msworker/MsWorkerApplication.java
@@ -1,13 +1,32 @@
 package com.coursespring.msworker;
 
+import com.coursespring.msworker.entities.Worker;
+import com.coursespring.msworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class MsWorkerApplication {
+public class MsWorkerApplication implements CommandLineRunner {
+
+    @Autowired
+    private WorkerRepository workerRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(MsWorkerApplication.class, args);
     }
 
+    @Override
+    public void run(final String... args) throws Exception {
+        final Worker worker1 = new Worker(null, "Wallace", 400.0);
+
+        final Worker worker2 = new Worker(null, "Joana", 600d);
+
+        final Worker worker3 = new Worker(null, "Beatris", 800d);
+
+        this.workerRepository.saveAll(Arrays.asList(worker1, worker2, worker3));
+    }
 }
