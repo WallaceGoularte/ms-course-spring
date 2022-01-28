3730c94c66330e118ad4a521353f1f3ede6d980d
Wallace Oliveira <wallace.goularte@hotmail.com>
2022-01-27 22:06:54 -0300

    Ajustes nos endpoints do ms-worker

--- ms-worker/src/main/java/com/coursespring/msworker/resources/WorkerResource.java
+++ ms-worker/src/main/java/com/coursespring/msworker/resources/WorkerResource.java
@@ -4,10 +4,7 @@ import com.coursespring.msworker.entities.Worker;
 import com.coursespring.msworker.services.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,17 +16,18 @@ public class WorkerResource {
     @Autowired
     private WorkerService workerService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
+
         return ResponseEntity.ok(workerService.findAll());
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Optional<Worker>> findById(@PathVariable final Long id){
         return ResponseEntity.ok(workerService.findById(id));
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public void save(final Worker worker){
         workerService.save(worker);
     }
